0db37ef4e7accf7761cdb4ccb8725b7eba2cb0ee
enriqueleyva <46473217+enriqueleyva@users.noreply.github.com>
2020-10-26 14:01:06 -0600

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1603741991,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/spr_boton-sheet0.png",
+		"images/particles.png",
+		"images/spr_secundario-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/spr_pregunta-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/btn_no-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
